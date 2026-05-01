a0c04cd8d508ce52c7465a801103f9a96aede528
naturebliss <naturebliss3468@gmail.com>
2026-05-01 17:26:58 +0530

    Delete vyom-ai directory

--- vyom-ai/next-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/navigation-types/navigation" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
